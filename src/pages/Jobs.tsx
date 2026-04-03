1dd6222083c025ac2707ef9b9d53f9803e7e4c27
Maria-Kcg <kpechegomariegrace@gmail.com>
2026-04-03 19:18:14 +0100

    barre de recherche

--- src/pages/Jobs.tsx
+++ src/pages/Jobs.tsx
@@ -3,16 +3,26 @@ import JobsCard from "../components/JobsCard";
 import { useState } from "react";
 
 function Jobs() {
+    const [search, setSearch] = useState(""); // état pour le mot-clé
+
     const [filteredSecteur, setFilteredSecteur] = useState("");
     const [filteredType, setFilteredType] = useState("");
     const [filteredLocation, setFilteredLocation] = useState("");
 
     const filteredJobs = jobs.filter((job) => {
-        return (
+        const filteredBar =
+            job.titre.toLowerCase().includes(search.toLowerCase()) ||
+            job.entreprise.toLowerCase().includes(search.toLowerCase()) ||
+            job.lieu.toLowerCase().includes(search.toLowerCase()) ||
+            job.type.toLowerCase().includes(search.toLowerCase()) ||
+            job.secteur.toLowerCase().includes(search.toLowerCase())
+
+        const filteredSelect =
             (filteredSecteur === "" || job.secteur === filteredSecteur) &&
             (filteredType === "" || job.type === filteredType) &&
-            (filteredLocation === "" || job.lieu === filteredLocation)
-        );
+            (filteredLocation === "" || job.lieu === filteredLocation);
+
+        return filteredBar && filteredSelect
     });
 
     return (
@@ -20,11 +30,17 @@ function Jobs() {
             <div className=" py-5 px-5 md:px-15 lg:px-35">
                 <div>
                     <p className="text-2xl font-bold text-emerald-600 text-center my-10">Trouve ton emploi au Bénin</p>
+
+                    <div className="flex justify-center mb-4">
+                        <input type="text" placeholder="Recherche par mot-clé Ex: entreprise, secteur..." value={search} onChange={(e) => setSearch(e.target.value)}
+                            className="border-2 border-emerald-600 bg-white rounded-full p-2 w-full md:w-1/2 mb-5"
+                        />
+                    </div>
                 </div>
 
-                <div className="flex flex-col md:flex-row items-center  justify-center md:justify-between  px-5  md: px-10 mb-5 bg-emerald-50 py-5">
+                <div className="flex flex-col md:flex-row items-center rounded-lg justify-center gap-10  px-5  md: px-10 mb-5 bg-emerald-500 py-5">
                     <select value={filteredSecteur} onChange={(e) => setFilteredSecteur(e.target.value)}
-                        className="border border-emerald-600 rounded-lg px-2 py-1"   >
+                        className="border border-emerald-600 bg-white rounded-lg p-2"   >
                         <option value="">Tous les secteurs</option>
                         <option value="Informatique">Informatique</option>
                         <option value="Marketing">Marketing</option>
@@ -38,7 +54,7 @@ function Jobs() {
                     </select>
 
                     <select value={filteredType} onChange={(e) => setFilteredType(e.target.value)}
-                        className="border border-emerald-600 rounded-lg  px-2 py-1"   >
+                        className="border border-emerald-600 bg-white rounded-lg  p-2"   >
                         <option value="">Tous les types</option>
                         <option value="CDI">CDI</option>
                         <option value="CDD">CDD</option>
@@ -46,7 +62,7 @@ function Jobs() {
                     </select>
 
                     <select value={filteredLocation} onChange={(e) => setFilteredLocation(e.target.value)}
-                        className="border border-emerald-600 rounded-lg  px-2 py-1"   >
+                        className="border border-emerald-600 bg-white rounded-lg  p-2"   >
                         <option value="">Toutes les localisations</option>
                         <option value="Cotonou, Bénin">Cotonou, Bénin</option>
                         <option value="Porto-Novo, Bénin">Porto-Novo, Bénin</option>
